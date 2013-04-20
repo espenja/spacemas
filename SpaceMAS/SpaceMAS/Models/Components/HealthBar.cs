8d02695e8754629678edfb0cde74a4e1565edf60
Espen Jacobsson <espenjacobsson@gmail.com>
2013-04-20 16:21:46 +0200

    Fixed issue with HealthPar SetPixels

--- SpaceMAS/SpaceMAS/Models/Components/HealthBar.cs
+++ SpaceMAS/SpaceMAS/Models/Components/HealthBar.cs
@@ -8,12 +8,13 @@ namespace SpaceMAS.Models.Components {
 
         private KillableGameObject Parent { get; set; }
         private float Percent { get; set; }
+        private Color HealthColor { get; set; }
 
         public HealthBar(KillableGameObject parent) {
             Parent = parent;
             Percent = 1;
             Texture = new Texture2D(GameServices.GetService<GraphicsDevice>(), 1, 1, false, SurfaceFormat.Color);
-            Texture.SetData(new[] { Color.LightGreen });
+            Texture.SetData(new[] { Color.White });
         }
 
         public override void Update(GameTime gameTime) {
@@ -30,17 +31,22 @@ namespace SpaceMAS.Models.Components {
 
             float percent = (Parent.HealthPoints / Parent.MaxHealthPoints);
 
-            if (percent > 0.7f) {
-                if (Percent <= 0.7f)
-                    Texture.SetData(new[] {Color.ForestGreen});
+            if (percent > 0.7f)
+            {
+                if (Percent >= 0.7f)
+                    //Texture.SetData(new[] {Color.ForestGreen});
+                    HealthColor = Color.LightGreen;
             }
-            else if(percent >= 0.4 && percent < 0.7) {
-                if(Percent >= 0.7 || Percent < 0.4)
-                    Texture.SetData(new[] { Color.OrangeRed });
+            else if (percent >= 0.4 && percent < 0.7)
+            {
+                if (Percent >= 0.7 || Percent < 0.4)
+                    //Texture.SetData(new[] { Color.OrangeRed });
+                    HealthColor = Color.Orange;
             }
-            else if(percent < 0.4)
-                if(Percent > 0.4)
-                    Texture.SetData(new[] { Color.Red });
+            else if (percent < 0.4)
+                if (Percent > 0.4)
+                    //Texture.SetData(new[] { Color.Red });
+                    HealthColor = Color.DarkRed;
 
             Percent = percent;
             base.Update(gameTime);
@@ -50,7 +56,7 @@ namespace SpaceMAS.Models.Components {
             Vector2 anchor = Parent.UpperLeftCorner();
             double healthBarWidth = Parent.Width * Percent;
 
-            Drawing.Draw(spriteBatch, Texture, new Rectangle((int)anchor.X, (int)anchor.Y - 10, (int)healthBarWidth, 3), Color.White, GameDrawOrder.GAME_LEVEL_TOP);
+            Drawing.Draw(spriteBatch, Texture, new Rectangle((int)anchor.X, (int)anchor.Y - 10, (int)healthBarWidth, 3), HealthColor, GameDrawOrder.GAME_LEVEL_TOP);
         }
     }
 }
