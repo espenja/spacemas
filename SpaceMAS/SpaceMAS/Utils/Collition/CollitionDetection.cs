a2797895efb26131d6851851a834c5713467e5e7
Espen Jacobsson <espenjacobsson@gmail.com>
2013-04-23 15:13:51 +0200

    Starting to implement line collition detection

--- SpaceMAS/SpaceMAS/Utils/Collition/CollitionDetection.cs
+++ SpaceMAS/SpaceMAS/Utils/Collition/CollitionDetection.cs
@@ -90,5 +90,73 @@ namespace SpaceMAS.Utils.Collition {
             // No intersection found
             return false;
         }
+
+        //public static bool CheckPerPixelCollision(GameObject a, GameObject b) {
+
+        //    double start = DateTime.Now.Ticks;
+
+        //    /*
+        //     * We need to make the transform matrix that goes from A's transform
+        //     * to B's transform, because they most likely have different effects
+        //     * like scale, rotation, position, and more.
+        //     */
+        //    Matrix atob = a.Transform * Matrix.Invert(b.Transform);
+
+        //    /*
+        //     * Our main loop checks every single column and row of A's texture.
+        //     * We need to duplicate this for B.  However, since B has a different 
+        //     * transform, we need to transform a normal from A to B in order
+        //     * to do the proper checking in B's world
+        //     */
+        //    Vector2 stepX = Vector2.TransformNormal(Vector2.UnitX, atob);
+        //    Vector2 stepY = Vector2.TransformNormal(Vector2.UnitY, atob);
+
+        //    /*
+        //     * 0x0 in A will be different in B since we have different
+        //     * transform matrices.  We need to calculate the correct one
+        //     * for B's start by using the atob transform we created.
+        //     */
+        //    Vector2 iBPos = Vector2.Transform(Vector2.Zero, atob);
+
+        //    //Start from 0x0 on A, and the similar coordinates in B
+        //    for (int deltax = 0; deltax < a.Texture.Width; deltax++)
+        //    {
+        //        Vector2 bpos = iBPos;
+        //        for (int deltay = 0; deltay < a.Texture.Height; deltay++)
+        //        {
+        //            int bx = (int)bpos.X;
+        //            int by = (int)bpos.Y;
+
+        //            /*
+        //             * The values need to be within the texture dimensions.
+        //             * Otherwise, the program will throw an ArrayIndexOutOfBounds
+        //             * exception
+        //             */
+        //            if (bx >= 0 && bx < b.Texture.Width && by >= 0 &&
+        //                by < b.Texture.Height)
+        //            {
+        //                //CHANGE THE '> 150' TO '!= 0' WHEN YOU USE THIS CODE
+        //                //IF YOU HAVE FULLY TRANSPARENT PIXELS!
+        //                if (a.TextureData[deltax + deltay * a.Texture.Width].A != 0
+        //                    && b.TextureData[bx + by * b.Texture.Width].A != 0) {
+        //                    return true;
+        //                }
+        //            }
+
+        //            /*
+        //             * We are looping through every single pixel in column deltax
+        //             * for the A texture.  We need to increment the same thing 
+        //             * for B's texture.
+        //             */
+        //            bpos += stepY;
+        //        }
+        //        iBPos += stepX;
+        //    }
+
+        //    Console.WriteLine("FALSE: " + (DateTime.Now.Ticks - start));
+
+        //    return false;
+        //}
     }
+
 }
